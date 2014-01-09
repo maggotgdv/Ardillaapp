d935cc1824ee1029a73c2be87b4d969feec42003
alejandro aguirre kuoman <alejandro.aguirre.k@outlook.com>
2014-01-09 03:24:39 -0500

    Aplicacion Napo Android
    subo la nueva app de napo con todo operativo y el googleplayservices lib
    para poder poner el mapa ademas de estar ya el splash parte de la
    pantalla de inicio entre otras cosas, el codigo esta realizado en la api
    10 es decir el android 2.3.3

---
+++ Napo2/src/com/avellana/napo/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.avellana.napo;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class SplashScreen extends Activity {
+
+	private static final int TIME = 3 * 1000;// 4 seconds 
+	@Override 
+	protected void onCreate(Bundle savedInstanceState) { 
+		super.onCreate(savedInstanceState); setContentView(R.layout.activity_splash_screen); 
+		new Handler().postDelayed(new Runnable() { @Override public void run() { 
+			Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+			startActivity(intent); SplashScreen.this.finish(); 
+			overridePendingTransition(R.anim.fade_in, R.anim.fade_out); } }, TIME);
+		new Handler().postDelayed(new Runnable() { 
+			@Override public void run() { } }, TIME); 
+		} 
+	@Override 
+	public void onBackPressed() { 
+		this.finish(); 
+		super.onBackPressed(); 
+		} 
+
+}
